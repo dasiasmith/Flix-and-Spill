4bb06c95d7c09e77aa5ab5c5a135cb1c7215cf94
Jing Liu <jingliu778@gmail.com>
2022-02-12 10:38:24 -0500

    merge

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -53,7 +53,7 @@ router.get("/review/:id", async (req, res) => {
 router.get("/profile", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
+    const userData = await Profile.findByPk(req.session.user_id, {
       attributes: { exclude: ["password"] },
       include: [{ model: Review }],
     });
