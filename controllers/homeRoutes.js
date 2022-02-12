5143e5060e5a7bfbe663708a17a1af88d169fe54
Jing Liu <jingliu778@gmail.com>
2022-02-12 13:24:48 -0500

    user sign up, sign in , sign out

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,6 +69,7 @@ router.get("/profile", withAuth, async (req, res) => {
   }
 });
 
+// login route
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
