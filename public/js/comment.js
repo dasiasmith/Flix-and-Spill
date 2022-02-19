601e83806df6b54eedb51f1094cb4986e3cacd1a
Dasia <dasiasmith023@gmail.com>
2022-02-18 22:21:09 -0600

    Fixed whatever went on with the comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,5 +1,4 @@
 const newFormHandler = async (event) => {
-<<<<<<< HEAD
     event.preventDefault();
     const comment = document.querySelector("#comment-text").value.trim();
     const review_id = window.location.toString().split("/")[
@@ -24,33 +23,3 @@ const newFormHandler = async (event) => {
   document
     .querySelector(".new-comment-form")
     .addEventListener("submit", newFormHandler);
-=======
-  event.preventDefault();
-
-  const comment = document.querySelector("#comment-text").value.trim();
-  const review_id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
-
-  console.log("comment------>", comment, review_id);
-  if (comment) {
-    const Comment = await fetch("/api/comments", {
-      method: "POST",
-      body: JSON.stringify({ comment, review_id }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (Comment.ok) {
-      document.location.reload();
-    } else {
-      alert("Failed to create comment");
-    }
-  }
-};
-
-document
-  .querySelector(".new-comment-form")
-  .addEventListener("submit", newFormHandler);
->>>>>>> 774b3e66250ba7fe2db0bd869b20f66fd17854e7
