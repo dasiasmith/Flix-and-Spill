65623eba4db86fd5d5e9a53fa573abbf8830c139
Dasia <dasiasmith023@gmail.com>
2022-02-18 22:02:20 -0600

    Edits to homepagehandlebars and styling

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,27 +1,25 @@
-const { Comment } = require("express");
-
 const newFormHandler = async (event) => {
     event.preventDefault();
-    
-    const comment_text = document.querySelector('#comment-text').value.trim();
-    if (comment_text) {
-        const Comment = await fetch('/api/comments',{
-            method: 'POST',
-            body: JSON.stringify({ comment_text }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        
-        if (Comment.ok) {
-            document.location.replace('/')
-        } else {
-            alert('Failed to create comment')
-        }
+    const comment = document.querySelector("#comment-text").value.trim();
+    const review_id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+    console.log("comment------>", comment, review_id);
+    if (comment) {
+      const Comment = await fetch("/api/comments", {
+        method: "POST",
+        body: JSON.stringify({ comment, review_id }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (Comment.ok) {
+        document.location.reload();
+      } else {
+        alert("Failed to create comment");
+      }
     }
-
-}
-
-document 
-.querySelector('.new-comment-form')
-.addEventListener('submit', newFormHandler);
+  };
+  document
+    .querySelector(".new-comment-form")
+    .addEventListener("submit", newFormHandler);
